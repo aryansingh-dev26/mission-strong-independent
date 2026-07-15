33cbe0e44564ee34535354365c7c9a86d107addd
aryansingh-dev26 <singharyan2872006@gmail.com>
2026-07-15 22:22:27 +0530

    Create Grade_Marking.py

---
+++ Day-03/Grade_Marking.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your Marks: "))
+
+if marks >= 90:
+    print("Final Result: Grade A+")
+
+elif marks >=75:
+    print("Final Result: Grade B+")    
+
+else : 
+   print("Final Result: Grade C+") 
